a25d31e2e3124427ce088aeb1fabc1870ab54dbc
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-19 21:09:20 -0500

    Create utils file and copy the 2 functions into it.

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+const apiUrl = `http://www.omdbapi.com`;
+const apiKey = '';
+
+export const getMoviesbySearchTerm = async term => {
+  const fetchResults = await fetch(`${apiUrl}/?apiKey=${apiKey}&t=${term}`);
+  const fetchData = await fetchResults.json();
+  return fetchData;
+}
+
+export const getMoviedetailsById = async movieId => {
+  const fetchResults = await fetch(`${apiUrl}/?apiKey=${apiKey}&i=${movieId}`);
+  const fetchData = await fetchResults.json();
+  return fetchData;
+}
+
