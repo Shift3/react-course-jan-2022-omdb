9debcaef3c19d855ff06470a9571db43b318069b
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-24 18:13:56 -0500

    feat(component): Create Movie Card functional component

---
+++ src/MovieCard.js
@@ -0,0 +1,11 @@
+const MovieCard = ({title, type, posterUrl}) => {
+  return (
+    <div>
+      <img src={posterUrl} alt={title} />
+      <p>{title}</p> 
+      <button>{type}</button> 
+    </div>
+  );
+}
+
+export default MovieCard;
